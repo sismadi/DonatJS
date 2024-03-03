23c5795182ee9e5f5f1e4ca92598c573a0f2cbaa
wawan sismadi <wawan@sismadi.com>
2024-03-03 09:45:51 +0700

    Add files via upload

--- app/donat/kasir.js
+++ app/donat/kasir.js
@@ -1,8 +1,13 @@
 kasir={
 model:{
-  versi:'1.0',
-  produk:[{id:1,nama:'bread',img:'img/bread.svg'},{id:2,nama:'tea',img:'img/tea.svg'},{id:3,nama:'donut',img:'img/donut.svg'}],
-  cart:[ {id:2,nama:'tea',img:'img/tea.svg'},{id:3,nama:'donut',img:'img/donut.svg'}],
+versi:'1.0',
+produk:[
+{id:1,nama:'pizza',img:'img/pizza.svg',banyak:1,harga:5000},
+{id:2,nama:'coffe',img:'img/coffee.svg',banyak:1,harga:5000},
+{id:3,nama:'donut',img:'img/donut.svg',banyak:1,harga:5000}],
+cart:[
+{id:2,nama:'coffee',img:'img/coffee.svg',banyak:1,harga:5000,jumlah:5000},
+{id:3,nama:'donut',img:'img/donut.svg',banyak:1,harga:5000,jumlah:5000}],
 
 },
 
@@ -14,10 +19,10 @@ color=d.color(step);
 if(step<1) color[0]='red';
 
 for(i in arr){ var {id,nama,img}=arr[i];
-out+=`<div class="col-1-${step}" onclick="${id}" >
+out+=`<div class="col-1-${step}" >
 <div class="ag" style="background:${color[i]}" > <img src="${img}" style="width:150px ; height:auto;"> <div class="ag-menu" >
 <div class="ag-title">${nama}<br/>Rp. 5.000</div>
-<div class="ag-desc"> <button onClick="kasir.controller.add(${id})">Add</button> </div>
+<div class="ag-desc"> <button onClick="kasir.controller.add(${i})">Add</button> </div>
 </div>
 </div>
 </div>`;
@@ -30,26 +35,28 @@ out=``;
 step=arr.length;
 color=d.color(step);
 if(step<1) color[0]='red';
-
-for(i in arr){ var {id,nama,img}=arr[i];
+out=`<div id="cart" class="row"  >`;
+for(i in arr){ var {id,nama,img,banyak,harga,jumlah}=arr[i];
 out+=`<div class="row shadow" onclick="${id}" >
 <div class="col-1-4" >
 <img src="${img}" style="width:50px ; height:auto;">
 </div>
 <div class="col-3-4" >
 <div class="">
-2 x ${nama} <br>
-@5.000<br/>
-Rp.10.000
+${banyak} x ${nama} <br>
+@${harga}<br/>
+Rp.${jumlah}
 </div>
 <div class="btns">
-<button class="" onClick="kasir.controller.add(${id})">${d.svg.icon('trush')} Remove</button>
-<button class="" onClick="kasir.controller.add(${id})">${d.svg.icon('plus')} Add</button>
+<button class="" onClick="kasir.controller.plus(${i})">${d.svg.icon('plus')}</button>
+<button class="" onClick="kasir.controller.minus(${i})">${d.svg.icon('minus')}</button>
+<button class="" onClick="kasir.controller.remove(${i})">${d.svg.icon('trush')}</button>
 </div>
 </div>
 </div>`;
 }
-out+=`<div class="shadow"> Total:Rp.20.000 <br/><button onClick="kasir.controller.add(${id})">Bayar</button> </div>`;
+out+=`<div class="shadow"> Total:Rp.<span id="total" >150.000</span> <br/><button onClick="kasir.controller.add(${id})">Bayar</button> </div>`;
+out+=`</div>`;
 return out;
 },
 
@@ -58,12 +65,53 @@ return out;
 },
 
 controller:{
-add:function(i){ d.modal(i);
+add:function(i){
+p=kasir.model.produk;
+id=p[i].id;
+c=kasir.model.cart;
+const nod = c.find(e => e.id === id);
+if(nod){nod.banyak+=1; }
+else{  c.push(p[i])}
+d.gebi('cart').innerHTML=kasir.view.cart(c);
+this.total();
+},
+
+remove:function(i){
+c=kasir.model.cart;
+kasir.model.cart.splice(i,1);
+d.gebi('cart').innerHTML=kasir.view.cart(c);
+this.total();
+
+},
+plus:function(i){
+c=kasir.model.cart;
+c[i].banyak+=1;
+d.gebi('cart').innerHTML=kasir.view.cart(c);
+this.total();
+
+},
+minus:function(i){
+c=kasir.model.cart;
+if(c[i].banyak>0){c[i].banyak-=1; }
+else { kasir.model.cart.splice(i,1);}
+d.gebi('cart').innerHTML=kasir.view.cart(c);
+this.total();
+
+},
+
+total:function(){
+c=kasir.model.cart;
+sum=0;
+for (i in c) {
+c[i].jumlah= (c[i].banyak * c[i].harga);
+sum+=c[i].jumlah;
+}
+d.gebi('cart').innerHTML=kasir.view.cart(c);
+d.gebi('total').innerHTML=sum;
 },
 produk:function(){
-  // d.modal('produk')
-  arr=kasir.model.produk;
-  d.gebi('content').innerHTML=kasir.view.produk(arr);
+arr=kasir.model.produk;
+d.gebi('content').innerHTML=kasir.view.produk(arr);
 
 }
 }
