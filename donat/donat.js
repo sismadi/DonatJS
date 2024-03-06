70e43a57b955b4972d652b7e4d6fcf2d8d8f9cb1
wawan sismadi <wawan@sismadi.com>
2024-03-06 20:57:39 +0700

    Add files via upload

--- donat/donat.js
+++ donat/donat.js
@@ -63,7 +63,7 @@ data:[{id:1,nama:"satu"},{id:2,nama:"dua"},],
 nama:'donat',
 css:{
 ul:'ul', li:'li', a:'a',search:'search',page:'shadow row',
-table:'table table-borderless report-table',
+table:'table',
 form:'form row',
 gInput:'gInput', input:'input', label:'label',
 gButton:"gButton", button:'button',
@@ -78,6 +78,10 @@ input:{id:'input',data:data,tipe:[
 ],},
 button:{id:'button',data:data,},
 block:{id:'block',data:[],},
+card:{id:'card',data:[
+  {id:1,nama:"User",isi:"2",icon:"person",url:"d.api.read('master_users/view')"},
+  {id:1,nama:"Menu",isi:"2",icon:"menu",url:"d.api.read('master_menu/view')"},
+]},
 view:{id:'view',
 datatable:datatable,
 
@@ -86,8 +90,9 @@ button:{data:[
 {id:2,nama:"Print",icon:"printer",url:"d.controller.print()"},
 ]},
 
-},
 
+
+},
 add:{
 id:'add',
 input:{data:data[0],tipe:[],},
@@ -125,9 +130,6 @@ card:{data:[
 },
 
 },
-
-
-
 
 
 view:{
@@ -186,20 +188,20 @@ var{page,rpp,data}=arr;
 var al=data.length;
 var np=Math.ceil(al / rpp);
 out=`<table class="${css.table}"><tr>`;
-out+=`<th>Aksi</th>`;
-for(i in data[0]){ out+=`<th>${i}</th>`; }
-out+=`</tr>`;
+out+=`<thead><th>Aksi</th>`;
+for(i in data[0]){ out+=`<th scope="col">${i}</th>`; }
+out+=`</tr></thead><tbody>`;
 
 for (var key=(page-1) * rpp; key < (page * rpp) && key < al; key++) { col=data[key];
-out+=`<tr">`;
+out+=`<tr>`;
 out+=`<td>  ${d.view.aksi(key)} </td>`;
 for(i in col){ str=col[i];
 
 if(str.length>12){ str=str.substring(0, 12) + ' ...' }
-out+=`<td> ${str}</td>`; }
+out+=`<td data-title="${i}">&nbsp; ${str} </td>`; }
 out+=`</tr>`;
 }
-out+=`</table>`;
+out+=`</tbody></table>`;
 return out;
 },
 
@@ -256,7 +258,7 @@ button:function(arr){
 var {data}=arr;
 out=`<div class="${css.gButton}">`;
 for(i in data){ var {icon,url,nama}=data[i];
-out+=`<button class="${css.button}" onclick="${url}"> ${d.svg.icon(icon)} <span> ${nama} </span></button>`; }
+out+=`<button class="${css.button}" onclick="${url}"> ${d.svg.icon(icon)}  ${nama}</button>`; }
 out+=`</div>`;
 return out;
 },
@@ -499,11 +501,6 @@ gradien:`<defs>
 </linearGradient>
 </defs>`,
 ktupad:`M16 3 19 6 15 10 12 7ZM8 10 11 7 15 11 12 14ZM16 10 19 7 23 11 20 14ZM8 3 11 6 7 10 4 7ZM0 10 3 7 7 11 4 14ZM8 18 11 15 15 19 12 22ZM8 11 11 14 7 18 4 15ZM16 11 19 14 15 18 12 15Z`,
-bayam:`M1 18 1 6A1 1 0 0111 6L11 13 18 13A1 1 0 0118 23L6 23 6 6A1 1 0 0116 6L16 8 18 8A1 1 0 0118 18Z`,
-donat:`M12 10C7 5 4 14 12 17 20 14 17 5 12 10M12 22A8 8 0 0112 2 8 8 0 0112 22Z`,
-twitter:`M23 3A11 11 0 0120 4 4 4 0 0012 7V8A10 10 0 013 4S1 14 8 17A12 12 0 011 19C10 24 21 19 21 8A4.5 4.5 0 0021 7 8 8 0 0023 3Z`,
-facebook:`M17 2H14A5 5 0 009 7V10H6V14H9V22H13V14H16L17 10H13V7A1 1 0 0114 6H17Z`,
-whatsapp:`M20 11A8 8 0 018 18L3 21 5 15A8 8 0 014 11 1 1 0 0120 11Z`,
 filter:`M11 12 14 12 14 22 11 22ZM4 2 7 2 7 11 4 11 4 2M11 2 14 2 14 7 16 7 16 10 9 10 9 7 11 7 11 2M18 2 21 2 21 13 18 13 18 2M2 13 9 13 9 16 7 16 7 22 4 22 4 16 2 16 2 13M16 15 23 15 23 18 21 18 21 22 18 22 18 18 16 18Z`,
 // filter:`M4 21 4 14M4 10 4 3M12 21 12 12M12 8 12 3M20 21 20 16M20 12 20 3M1 14 7 14M9 8 15 8M17 16 23 16`,
 person:`M2 22V19A4 4 0 015 15H19A4 4 0 0122 19V22ZM7 7A4 4 0 0017 7 4 4 0 007 7`,
@@ -521,11 +518,15 @@ cart:`M4 5 2 5 2 2 5 2 8 7 22 6 19 18 11 18ZM9 21A1 1 0 0013 21 1 1 0 009 21M16 
 card:`M5 16H7M2 9H22M2 5H22V19H2Z`,
 envelope:`M2 8C2 7 3 6 4 6L20 6C21 6 22 7 22 8L22 20C22 21 21 22 20 22L4 22C3 22 2 21 2 20ZM22 8 12 15 2 8`,
 geo:`M4 10A1 1 0 0120 10C20 17 12 22 12 22 12 22 4 17 4 10M9 10A1 1 0 0015 10 1 1 0 009 10`,
+twitter:`M23 3A11 11 0 0120 4 4 4 0 0012 7V8A10 10 0 013 4S1 14 8 17A12 12 0 011 19C10 24 21 19 21 8A4.5 4.5 0 0021 7 8 8 0 0023 3Z`,
+facebook:`M17 2H14A5 5 0 009 7V10H6V14H9V22H13V14H16L17 10H13V7A1 1 0 0114 6H17Z`,
+whatsapp:`M20 11A8 8 0 018 18L3 21 5 15A8 8 0 014 11 1 1 0 0120 11Z`,
 eye:`M1 12S5 4 12 4S23 12 23 12S19 20 12 20S1 12 1 12ZM9 12A3 3 0 1 0 15 12A3 3 0 1 0 9 12`,
 // qrcode:`M2 2 11 2 11 11 2 11ZM14 2 23 2 23 9 14 9ZM2 14 11 14 11 20 2 20ZM15 14 23 14 23 21 15 21Z`,
 qrcode:`M2 2 8 2 8 8 2 8ZM16 2 22 2 22 8 16 8ZM2 16 8 16 8 22 2 22ZM12 12 22 12 22 22 12 22z`,
 search:`M20 11A1 1 0 002 11 1 1 0 0020 11M5 11A1 1 0 0117 11 1 1 0 015 11ZM17 19 19 17 24 22 22 24Z`,
 minus:`M2 12a10 10 0 1020 0 10 10 0 10-20 0M7 11 17 11 17 13 7 13Z`,
+bayam:`M1 18 1 6A1 1 0 0111 6L11 13 18 13A1 1 0 0118 23L6 23 6 6A1 1 0 0116 6L16 8 18 8A1 1 0 0118 18Z`,
 scan:`M2 10 2 5C2 3 3 2 5 2L10 2 10 5 6 5C5 5 5 5 5 6L5 10ZM15 2 19 2C21 2 22 3 22 5L22 10 19 10 19 6C19 5 19 5 18 5L15 5ZM22 19C22 21 21 22 19 22L15 22 15 19 18 19C19 19 19 19 19 18L19 15 22 15ZM10 22 5 22C3 22 2 21 2 19L2 15 5 15 5 18C5 19 5 19 6 19L10 19Z`,
 upload:`M5 8 12 2 19 8 19 12 12 6 5 12ZM5 14 2 14 2 19C2 21 3 22 5 22L19 22C21 22 22 21 22 19L22 14 19 14 19 18C19 19 19 19 18 19L6 19C5 19 5 19 5 18Z`,
 download:`M5 2 12 8 19 2 19 6 12 12 5 6ZM5 14 2 14 2 19C2 21 3 22 5 22L19 22C21 22 22 21 22 19L22 14 19 14 19 18C19 19 19 19 18 19L6 19C5 19 5 19 5 18Z`,
@@ -540,13 +541,12 @@ io:`M5 7 1 17 19 17 23 7Z`,
 connector:`M18 12A1 1 0 006 12 1 1 0 0018 12Z`,
 ellipse:`M1 12C1 22 23 22 23 12 23 2 1 2 1 12`,
 calendar:`M2 5Q2 3 4 3A1 1 0 016 3L18 3A1 1 0 0120 3Q22 3 22 5L22 19Q22 22 19 22L5 22Q2 22 2 19ZM4 8 20 8 20 19A1 1 0 0119 20L5 20A1 1 0 014 19Z`,
+donat:`M12 10C7 5 4 14 12 17 20 14 17 5 12 10M12 22A8 8 0 0112 2 8 8 0 0112 22Z`,
+file:`M4 2 13 2 20 9 20 22 4 22ZM20 9 13 9 13 2Z`,
 save:`M2 2 22 2 22 22 6 22 2 18ZM5 2 19 2 19 10 5 10ZM9 16 19 16 19 22 9 22Z`,
-share:`M7 13 13 13 17 18 19 17 15 12 19 7 17 6 13 11 7 11ZM16 5A1 1 0 0022 5 1 1 0 0016 5M2 12A1 1 0 008 12 1 1 0 002 12M16 19A1 1 0 0022 19 1 1 0 0016 19`,
-category:`M7 10 17 10 12 2ZM2 18A1 1 0 0010 18 1 1 0 002 18M14 14 22 14 22 22 14 22Z`,
-mark:`M4 2 20 2 20 22 12 18 4 22Z`,
-link:`M8 7 8 4 10 2 18 2 20 4 20 14 18 16 9 16 8 15 8 12 11 12 12 13 16 13 17 12 17 6 16 5 12 5 11 6 11 7ZM6 8 15 8 16 9 16 12 13 12 12 11 8 11 7 12 7 18 8 19 12 19 13 18 13 17 16 17 16 20 14 22 6 22 4 20 4 10Z`,
-google:`M2 12C2 7 7 2 12 2 17 2 19 5 19 5L16 8C16 8 14 6 12 6 9 6 6 9 6 12 6 15 9 18 12 18 15 18 18 15 18 14L12 14 12 10 22 10 22 12C22 17 18 22 12 22 7 22 2 17 2 12ZM2 22M12 6M12 2`,
-wedus:`M2 10 6 2 8 3 12 2 10 4 8 3 12 5 2 10 10 6 13 10 8 22 13 10 16 10 15 15 16 10 19 10 20 12 21 12 22 15 20 14 20 12 20 22 15 15 8 22 8 7 8 10 6 10 4 13 4 10 6 10Z`,
+kinjeng:`M9 4C9 2 12 2 12 4 12 2 15 2 15 4 15 8 12 5 12 4 12 5 9 8 9 4M12 8C14 9 23 12 23 7 23 5 17 8 12 8 20 7 23 6 23 1 22-2 19 3 12 8M12 6C11 6 10 6 11 10 11 13 11 22 12 22 13 22 13 13 13 10 14 6 13 6 12 6M12 8C5 3 2-2 1 1 1 6 5 7 12 8 7 8 1 5 1 7 1 12 10 9 12 8`,
+
+
 },
 
 icon:function(id='bayam'){
